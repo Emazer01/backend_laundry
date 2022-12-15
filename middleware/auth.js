741984d1b046967eb09cfee0cad0821ab04c02b7
Emazer01 <garyferdinand15@gmail.com>
2022-12-15 21:25:33 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ SECRET = process.env.SECRET
 
 const Auth = {
     verifyToken(req, res, next){
-      console.log('mulai auth')
         const token = req.body.token
         if (!token) {
           return res.status(403).send("A token is required for authentication");
@@ -13,7 +12,6 @@ const Auth = {
         try{
           var verified = jwt.verify(token, SECRET);
           req.user = verified
-          console.log('selesai auth')
           return next()
         } catch(err){
           res.status(403).send('Youre not authenticated, please login first')
